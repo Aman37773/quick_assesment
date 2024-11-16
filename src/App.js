1aeded5c370529aa4935ee9b5aafecf4f4847544
Aman37773 <153506991+Aman37773@users.noreply.github.com>
2024-11-16 13:22:42 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Aman from './components/Aman';
+import Title from './components/Title';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Aman/>
+      <Title/>
+    </div>
+  );
+}
+
+export default App;
